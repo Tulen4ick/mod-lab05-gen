d6ef14b2444cf129bbc897dac4c72b6cf90db313
Tulen4ick <zaharsazanov2015@gmail.com>
2025-04-28 00:00:55 +0300

    Completing both tasks + comparison charts

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,44 +1,168 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace generator
 {
-    class CharGenerator 
+    public class CharGenerator
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
+        private List<string> syms = new List<string>();
+        private List<int> weights = new List<int>();
+        public readonly int summ;
         private Random random = new Random();
-        public CharGenerator() 
+        public CharGenerator(string charsfile)
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            var currentDir = Directory.GetCurrentDirectory();
+            var directory = new DirectoryInfo(currentDir);
+            while (directory != null && !directory.GetFiles("*.csproj").Any())
+            {
+                directory = directory.Parent;
+            }
+            var bigramsAndWeights = File.ReadAllLines(Path.Combine(directory.FullName, charsfile));
+            foreach (var bigraminfo in bigramsAndWeights)
+            {
+                var parts = bigraminfo.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var bigram = parts[1].Trim();
+                syms.Add(bigram);
+                var weight = int.Parse(parts[2].Trim());
+                weights.Add(weight);
+                summ += weight;
+            }
         }
-        public char getSym() 
+        public string getSym()
         {
-           return data[random.Next(0, size)]; 
+            var indice = random.Next(0, summ);
+            int sum = 0;
+            for (int i = 0; i < weights.Count; ++i)
+            {
+                sum += weights[i];
+                if (indice <= sum)
+                {
+                    return syms[i];
+                }
+            }
+            return "";
+        }
+        public int GetSize()
+        {
+            return syms.Count;
+        }
+        public int GetBigrammWeight(string bigram)
+        {
+            return weights[syms.IndexOf(bigram)];
+        }
+    }
+
+    public class WordGenerator
+    {
+        private List<string> words = new List<string>();
+        private List<double> weights = new List<double>();
+        public readonly double summ;
+        private Random random = new Random();
+        public WordGenerator(string wordsfile)
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var directory = new DirectoryInfo(currentDir);
+            while (directory != null && !directory.GetFiles("*.csproj").Any())
+            {
+                directory = directory.Parent;
+            }
+            var wordsAndWeights = File.ReadAllLines(Path.Combine(directory.FullName, wordsfile));
+            foreach (var wordinfo in wordsAndWeights)
+            {
+                var parts = wordinfo.Replace('.', ',').Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var word = parts[1].Trim();
+                words.Add(word);
+                var weight = double.Parse(parts[4].Trim());
+                weights.Add(weight);
+                summ += weight;
+            }
+        }
+        public string getSym()
+        {
+            int indice = random.Next(0, (Int32)summ);
+            double sum = 0;
+            for (int i = 0; i < weights.Count; ++i)
+            {
+                sum += weights[i];
+                if (indice <= (Int32)sum)
+                {
+                    return words[i];
+                }
+            }
+            return "";
+        }
+        public int GetSize()
+        {
+            return words.Count;
+        }
+        public double GetWordWeight(string word)
+        {
+            return weights[words.IndexOf(word)];
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            var currentDir = Directory.GetCurrentDirectory();
+            var directory = new DirectoryInfo(currentDir);
+            while (directory != null && !directory.GetFiles("*.csproj").Any())
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                directory = directory.Parent;
+            }
+            CharGenerator genBi = new CharGenerator("bigramsAndWeights.txt");
+            SortedDictionary<string, int> statBi = new SortedDictionary<string, int>();
+            string result = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                string ch = genBi.getSym();
+                result += ch;
+                if (statBi.ContainsKey(ch))
+                    statBi[ch]++;
+                else
+                    statBi.Add(ch, 1); Console.Write(ch);
             }
             Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            var resultPath1 = Path.Combine(directory.Parent.FullName, "Results", "gen-1.txt");
+            File.WriteAllText(resultPath1, result, Encoding.UTF8);
+
+            WordGenerator genWord = new WordGenerator("wordsAndWeights.txt");
+            SortedDictionary<string, int> statWord = new SortedDictionary<string, int>();
+            result = "";
+            for (int i = 0; i < 1000; i++)
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                string ch = genWord.getSym();
+                result += ch + " ";
+                if (statWord.ContainsKey(ch))
+                    statWord[ch]++;
+                else
+                    statWord.Add(ch, 1); Console.Write(ch);
             }
-            
+            Console.Write('\n');
+            var resultPath2 = Path.Combine(directory.Parent.FullName, "Results", "gen-2.txt");
+            File.WriteAllText(resultPath2, result, Encoding.UTF8);
+
+            var graphicData1 = Path.Combine(directory.Parent.FullName, "Results", "bigramms_data.txt");
+            List<string> dataBi = new List<string>();
+            foreach (KeyValuePair<string, int> entry in statBi)
+            {
+                string data = entry.Key + " " + (entry.Value / 1000.0).ToString() + " " + ((Double)genBi.GetBigrammWeight(entry.Key) / genBi.summ).ToString();
+                dataBi.Add(data);
+            }
+            File.WriteAllLines(graphicData1, dataBi, Encoding.UTF8);
+
+            var graphicData2 = Path.Combine(directory.Parent.FullName, "Results", "words_data.txt");
+            List<string> dataWord = new List<string>();
+            foreach (KeyValuePair<string, int> entry in statWord)
+            {
+                string data = entry.Key + " " + (entry.Value / 1000.0).ToString() + " " + ((Double)genWord.GetWordWeight(entry.Key) / genWord.summ).ToString();
+                dataWord.Add(data);
+            }
+            File.WriteAllLines(graphicData2, dataWord, Encoding.UTF8);
         }
     }
 }
---
+++ Results/words_plot.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+
+matplotlib.rc('font', family='Verdana')
+
+words = []
+real_probability = []
+expected_probability = []
+with open('Results/words_data.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        values = line.replace(',','.').strip().split(' ')
+        words.append(values[0])
+        real_probability.append(float(values[1]))
+        expected_probability.append(float(values[2]))
+
+
+words = words[:100]
+real_probability = real_probability[:100]
+expected_probability = expected_probability[:100]
+
+bar_width = 0.35
+index = np.arange(len(words))
+
+plt.figure(figsize = (25, 15))
+ax = plt.subplot()
+
+bars1 = ax.bar(index - bar_width/2, real_probability, bar_width, label = 'Реальные значения', color = 'skyblue')
+bars2 = ax.bar(index + bar_width/2, expected_probability, bar_width, label = 'Ожидаемые значения', color = 'orange')
+
+ax.set_title('Сравнение вероятностей биграмм', fontsize=19)
+ax.set_xlabel('Слова')
+ax.set_ylabel('Вероятности')
+ax.set_xticks(index)
+ax.set_xticklabels(words, rotation=90, ha='center', va='top', fontsize=9)
+for xtick in index:
+    ax.axvline(xtick, color='gray', alpha=0.1, linestyle='--')
+ax.legend(fontsize=12)
+
+plt.grid(axis='y', alpha=0.75)
+
+plt.tight_layout()
+
+plt.show()
+
+
